bd080815b77781cdc563db4794d383026d7ee0f0
Luan Oliveira <9741553+luanelioliveira@users.noreply.github.com>
2023-05-23 07:45:29 -0300

    first commit

---
+++ src/main/java/dev/luanoliveira/foodest/FoodestApplication.java
@@ -0,0 +1,13 @@
+package dev.luanoliveira.foodest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/luanoliveira/foodest/FoodestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luanoliveira.foodest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
